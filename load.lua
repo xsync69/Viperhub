242c3aff59274e77142635caea6876ff80cb9a0c
xsync69 <145942743+xsync69@users.noreply.github.com>
2023-12-06 22:45:45 +0000

    viperload.lua

---
+++ load.lua
@@ -0,0 +1,10 @@
+if game.PlaceId == 3956818381 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/xsync69/Viperhub/main/NinjaLegends%20My%20OBF.lua"))()
+  elseif game.PlaceId == 8540346411 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xsync69/Viperhub/main/RebirthChampionsX%20My%20OBF.lua"))()
+ elseif game.PlaceId == 1962086868 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xsync69/Viperhub/main/TowerOfHell%20My%20OBF.lua"))()
+ else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xsync69/Viperhub/main/Universal%20My%20OBF.lua"))()
+end
+  
